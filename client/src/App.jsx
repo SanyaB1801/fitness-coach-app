56cd30c63b05c7211915ae0a37f7fbe4a751fa65
UTSAV SINGHAL <119779889+UTSAVS26@users.noreply.github.com>
2024-11-01 12:21:15 +0530

    Login/Register Page

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import ProtectedPage from './pages/Protected';
+import Register from './pages/Register';
+
+function App() {
+
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/protected" element={<ProtectedPage />} />
+                <Route path="/register" element={<Register />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
