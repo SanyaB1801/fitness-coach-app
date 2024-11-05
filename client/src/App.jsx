c15cfe52e67a25c66e46d46975a7af318b7011bb
SanyaB1801 <sanyabehera13@gmail.com>
2024-11-05 19:26:35 +0530

    Updated client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import ProtectedPage from './pages/Protected';
 import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import HomePage from './pages/Homepage';
 
 function App() {
 
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Login />} />
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<Login />} />
                 <Route path="/protected" element={<ProtectedPage />} />
                 <Route path="/register" element={<Register />} />
+                <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
         </Router>
     );
